6dd1045d45583acb453906d52f1bdbc294b2f6ea
Danila846 <64487086+Danila846@users.noreply.github.com>
2021-08-29 01:43:08 +0700

    Add files via upload

---
+++ quiz-app/script.js
@@ -0,0 +1,103 @@
+const quizData = [
+	{
+		question: 'How old is Tom?',
+		a: '10',
+		b: '17',
+		c: '26',
+		d: '35',
+		correct: 'c'
+	}, {
+		question: 'What is the most used programming language in 2021?',
+		a: 'Python',
+		b: 'Java',
+		c: 'C++',
+		d: 'JavaScript',
+		correct: 'd'
+	}, {
+		question: 'Who is the President of US?',
+		a: 'Joe Biden',
+		b: 'Ivan Saldano',
+		c: 'Florin pop',
+		d: 'Donald Trump',
+		correct: 'a'
+	}, {
+		question: 'What does HTML stand for?',
+		a: 'Hypertext Markup Language',
+		b: 'Cascading Style Sheet',
+		c: 'Jason Object Notation',
+		d: 'Application Programming Interface',
+		correct: 'a'
+	}, {
+		question: 'What year was JavaScript launched?',
+		a: '1996',
+		b: '1995',
+		c: '1994',
+		d: 'None of the above',
+		correct: 'b'
+	}
+];
+
+const quiz = document.getElementById('quiz');
+const answersEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const button = document.getElementById('btn');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+	deselectAnswers();
+
+	const currentQuizData = quizData[currentQuiz];
+
+	questionEl.innerText = currentQuizData.question;
+	a_text.innerText = currentQuizData.a;
+	b_text.innerText = currentQuizData.b;
+	c_text.innerText = currentQuizData.c;
+	d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+
+	let answer = undefined;
+
+	answersEls.forEach((answerEl) => {
+		if (answerEl.checked) {
+			answer = answerEl.id;
+		}
+	});
+
+	return answer;
+
+}
+
+function deselectAnswers() {
+	answersEls.forEach((answerEl) => {
+		answerEl.checked = false;
+	});
+}
+
+button.addEventListener('click', () => {
+	const answer = getSelected();
+	if (answer) {
+		if (answer === quizData.correct) {
+			score++;
+		}
+		currentQuiz++;
+		if (currentQuiz < quizData.length) {
+			loadQuiz();
+		} else {
+			quiz.innerHTML = `
+			<h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+			<button class="btn" onClick="location.reload()">Reload</button>
+			`;
+		}
+	}
+
+})
